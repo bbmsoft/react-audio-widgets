cb7a7ff7d84ca5aa5d2ccc327414378d9359284e
Michael Bachmann <mbachmann@bbmsoft.net>
2021-03-05 00:46:33 +0100

    synchronizing on backend

--- src/App.js
+++ src/App.js
@@ -1,72 +1,148 @@
 import './App.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ParametricEQ, { BandType } from './components/ParametricEQ';
 import Canvas from './components/Canvas';
 import SliderEQ from './components/SliderEQ';
 import { Button } from '@material-ui/core';
 import KnobEqBand from './components/KnobEqBand';
 
+const BACKEND_ADDRESS = "ws://bbmsoft.net:9021/";
+
+const INITIAL_EQ = {
+  minFreq: 200,
+  maxFreq: 24000,
+  minGain: -12.0,
+  maxGain: 12.0,
+  minQ: 0.1,
+  maxQ: 100.0,
+  activeBand: 0,
+  bands: [
+    {
+      type: BandType.BELL,
+      gain: 0.0,
+      frequency: 400,
+      q: 1,
+    }, {
+      type: BandType.BELL,
+      gain: 0.0,
+      frequency: 1200,
+      q: 1,
+    }, {
+      type: BandType.BELL,
+      gain: 0.0,
+      frequency: 3600,
+      q: 1,
+    }, {
+      type: BandType.BELL,
+      gain: 0.0,
+      frequency: 10800,
+      q: 1,
+    }
+  ]
+}
+
+function connectToWs(updateEq, setWS) {
+  let socket = new WebSocket(BACKEND_ADDRESS);
+
+  socket.onclose = e => {
+    alert("Backend connection lost. Click OK to reconnect!");
+    setTimeout(() => connectToWs(updateEq, setWS), 500);
+  };
+
+  socket.onopen = function (e) {
+    console.info("Connected to backend.");
+  };
+
+  socket.onerror = function (error) {
+    console.error(error.message);
+    alert("Backend connection lost. Click OK to reconnect!");
+    setTimeout(() => connectToWs(updateEq, setWS), 500);
+  };
+
+  socket.onmessage = function (event) {
+    const eq = JSON.parse(event.data);
+    if (eq && eq.bands) {
+      updateEq(eq);
+    }
+  };
+
+  setWS(socket);
+}
+
 function App() {
 
-  const initialEq = {
-    minFreq: 200,
-    maxFreq: 24000,
-    minGain: -12.0,
-    maxGain: 12.0,
-    minQ: 0.1,
-    maxQ: 100.0,
-    activeBand: 0,
-    bands: [
-      {
-        type: BandType.BELL,
-        gain: 0.0,
-        frequency: 400,
-        q: 1,
-      }, {
-        type: BandType.BELL,
-        gain: 0.0,
-        frequency: 1200,
-        q: 1,
-      }, {
-        type: BandType.BELL,
-        gain: 0.0,
-        frequency: 3600,
-        q: 1,
-      }, {
-        type: BandType.BELL,
-        gain: 0.0,
-        frequency: 10800,
-        q: 1,
-      }
-    ]
+  const [eq, setEq] = useState(INITIAL_EQ);
+  const [ws, setWS] = useState(null);
+
+  window.localEq = eq;
+  window.ws = ws;
+
+  const eqReceived = receivedEq => {
+    const newEq = { ...receivedEq, activeBand: window.localEq.activeBand };
+    if (window.inputLocked) {
+      window.remoteEq = newEq;
+    } else {
+      setEq(newEq);
+    }
   }
 
-  const [eq, setEq] = useState(initialEq);
+  const onInput = newEq => {
+
+    if (window.timeout) {
+      clearTimeout(window.timeout);
+    }
+
+    window.timeout = setTimeout(() => {
+      window.inputLocked = false;
+      if (window.remoteEq) {
+        setEq(window.remoteEq);
+        window.remoteEq = null;
+      }
+    }, 1000);
+
+    window.inputLocked = true;
+
+    setEq({ ...newEq });
+
+    if (!window.sendPending) {
+      window.sendPending = setTimeout(() => {
+        window.sendPending = null;
+        window.ws && window.ws.send(JSON.stringify(window.localEq));
+      }, 17);
+    }
+  };
+
+  useEffect(() => {
+    connectToWs(eqReceived, setWS);
+  }, []);
+
+  const width = 1600;
 
   return (
     <div className="App" style={{
-      width: "1600px",
+      width: `${width}px`,
       margin: "0 auto",
       left: "50%"
     }}>
       <div>
-        <Canvas width={1600} height={600}>
+        <Canvas width={width} height={500}>
           <ParametricEQ
             eq={eq}
             id="mainEq"
             x={0}
             y={0}
-            width={1600}
-            height={600}
-            onUserInput={setEq}
+            width={width}
+            height={500}
+            onUserInput={onInput}
           />
         </Canvas>
         <SliderEQ
           eq={eq}
-          onUserInput={setEq}
+          onUserInput={onInput}
         />
       </div>
-      <KnobEqBands id={"knobEq"} eq={eq} onUserInput={setEq} />
+      <KnobEqBands id={"knobEq"} eq={eq} onUserInput={onInput} />
+      <div><Button variant="contained" onClick={() => onInput(INITIAL_EQ)}>Reset</Button></div>
     </div>
   );
 }

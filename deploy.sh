b7120de402a5639fbf19e038b14e133a130039d9
Michael Bachmann <mbachmann@bbmsoft.net>
2021-10-08 17:32:29 +0000

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker build -t babymotte/react-audio-widgets-demo . && docker push
+npm run build &&
+    docker build -t babymotte/react-audio-widgets-demo . &&
+    docker push babymotte/react-audio-widgets-demo
